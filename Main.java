1da63b8ae8db5e8850d52c567236ffc95cd0a957
KH <h.kikuchi.sys23@morijyobi.ac.jp>
2023-11-09 10:38:21 +0900

    身長、体重

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-public class Main{
-        public static void main(String[] args){
-                System.out.println("身長300.23cm");
-		System.out.println("体重20.48kg");
-        }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	 System.out.println("一人目");
+        double sincho = Double.parseDouble(sc.nextLine());
+        double taiju = Double.parseDouble(sc.nextLine());
+	 System.out.println("二人目");
+        double sincho2 = Double.parseDouble(sc.nextLine());
+        double taiju2 = Double.parseDouble(sc.nextLine());
+	 System.out.println("一人目");
+        System.out.println("身長" + sincho);
+        System.out.println("体重" + taiju);
+        System.out.println("二人目");
+        System.out.println("身長" + sincho2);
+        System.out.println("体重" + taiju2);
+    }
 }
